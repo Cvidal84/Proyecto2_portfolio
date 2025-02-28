742080a5b59128f33644446448ddf8b8261f58a2
Cvidal84 <carlosvidalpuigcerver@gmail.com>
2025-02-28 22:03:29 +0100

    arreglo
    arreglo cositas

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,8 +5,8 @@ export const Footer = () => `
 
 <h2>Contact</h2>
 <div>
-${Button("/icons/github.jpeg", "GitHub", "https://github.com/Cvidal84")}
-${Button("/icons/in.jpeg", "LinkedIn", "https://www.linkedin.com/in/carlos-vidal-puigcerver ")}
+${Button("/icons/github.png", "GitHub", "https://github.com/Cvidal84")}
+${Button("/icons/in.jpg", "LinkedIn", "https://www.linkedin.com/in/carlos-vidal-puigcerver ")}
 ${Button("/icons/telegram.png", "Telegram", "https://www.t.me/Carlosvidal")}
 ${Button("/icons/instagram.jpg", "Instagram", "https://www.instagram.com/carlosvidalpuigcerver/")}
 </div>
